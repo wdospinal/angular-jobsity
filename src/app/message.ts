1c1373944c56fd1ab14b76f1b6ff6676d02c2194
Wilson Daniel Ospina <wdospinal@unal.edu.co>
2018-06-14 15:12:02 -0500

    feat: message class

---
+++ src/app/message.ts
@@ -0,0 +1,9 @@
+export class Message {
+  constructor(
+    public id: string,
+    public dni: string,
+    public message: string,
+    public name: string,
+    public timestamp: string
+  ) {  }
+}
